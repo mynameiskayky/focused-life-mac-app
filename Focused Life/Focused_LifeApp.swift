cfeaaa74be061f3564b70eb761c39311b9efe650
nynameiskayky <kaykyvieiraa@hotmail.com>
2024-01-23 16:23:59 -0300

    Initial Commit

---
+++ Focused Life/Focused_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focused_LifeApp.swift
+//  Focused Life
+//
+//  Created by Kayky Vieira on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Focused_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Focused LifeUITests/Focused_LifeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Focused_LifeUITestsLaunchTests.swift
+//  Focused LifeUITests
+//
+//  Created by Kayky Vieira on 23/01/24.
+//
+
+import XCTest
+
+final class Focused_LifeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
